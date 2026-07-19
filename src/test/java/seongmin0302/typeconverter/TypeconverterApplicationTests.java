303511a0363eead70b08650e2c0b43b9591cc3a5
seongmin0302 <seongmin0302@naver.com>
2026-07-19 08:56:28 +0000

    project start

---
+++ src/test/java/seongmin0302/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package seongmin0302.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
